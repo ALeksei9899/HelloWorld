35b5e83c4a9cd1b78a1a6f24ceba5a2a408995d8
Aleksei9899 <Neu.bu@mail.ru>
2023-03-15 19:11:59 +0300

    Test

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -2,8 +2,6 @@ public class HelloWorld2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("egfergegeg");
-        System.out.println(
-
-        );
+        System.out.println("yugjyhghj");
     }
 }
